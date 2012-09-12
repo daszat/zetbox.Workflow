ab3dd334d96542ab3a661fb90fdf8bab59acb411
Arthur Zaczek <arthur@dasz.at>
2012-09-12 17:33:24 +0200

    Implement GetName correctly V2

--- zetbox.Workflow.Common/Workflow/ActionActions.cs
+++ zetbox.Workflow.Common/Workflow/ActionActions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Zetbox.API;
+using System.Text.RegularExpressions;
 
 namespace Zetbox.Basic.Workflow
 {
@@ -18,7 +19,14 @@ namespace Zetbox.Basic.Workflow
         [Invocation]
         public static void GetName(Action obj, MethodReturnEventArgs<string> e)
         {
-            e.Result = obj.Module != null ? string.Format("Workflow.Actions.{0}.{1}", obj.Module.Namespace, obj.Name) : null;
+            e.Result = obj.Module != null ? string.Format("Workflow.Actions.{0}.{1}", obj.Module.Namespace, Regex.Replace(obj.Name, "\\W", "_")) : null;
+        }
+
+        [Invocation]
+        public static void Execute(Action obj, Zetbox.Basic.Workflow.State current)
+        {
+            // Add logfile entry
+            // call invocation
         }
     }
 }
