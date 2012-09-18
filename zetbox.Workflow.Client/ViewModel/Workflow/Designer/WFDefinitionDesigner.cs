3f4f6b5dcf4889a35ba16527c4e767f6303a9f28
Arthur Zaczek <arthur@dasz.at>
2012-09-18 10:29:50 +0200

    Setting up graphical workflow designer

---
+++ zetbox.Workflow.Client/ViewModel/Workflow/Designer/WFDefinitionDesigner.cs
@@ -0,0 +1,30 @@
+﻿
+namespace zetbox.Workflow.Client.ViewModel.Workflow.Designer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Zetbox.Client.Presentables;
+    using Zetbox.API;
+    using wf = Zetbox.Basic.Workflow;
+
+    [ViewModelDescriptor]
+    public class WFDefinitionDesigner : ViewModel
+    {
+        public new delegate WFDefinitionDesigner Factory(IZetboxContext dataCtx, ViewModel parent, IDataObject obj);
+
+        public WFDefinitionDesigner(IViewModelDependencies appCtx, IZetboxContext dataCtx, ViewModel parent, wf.WFDefinition obj)
+            : base(appCtx, dataCtx, parent)
+        {
+            WFDefinition = obj;
+        }
+
+        public wf.WFDefinition WFDefinition { get; private set; }
+
+        public override string Name
+        {
+            get { return WFDefinition.ToString(); }
+        }
+    }
+}
---
+++ zetbox.Workflow.Client/ViewModel/Workflow/WFDefinitionViewModel.cs
@@ -0,0 +1,38 @@
+﻿
+namespace zetbox.Workflow.Client.ViewModel.Workflow
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Zetbox.Client.Presentables;
+    using Zetbox.API;
+    using wf = Zetbox.Basic.Workflow;
+    using zetbox.Workflow.Client.ViewModel.Workflow.Designer;
+
+    [ViewModelDescriptor]
+    public class WFDefinitionViewModel : DataObjectViewModel
+    {
+        public new delegate WFDefinitionViewModel Factory(IZetboxContext dataCtx, ViewModel parent, IDataObject obj);
+
+        public WFDefinitionViewModel(IViewModelDependencies appCtx, IZetboxContext dataCtx, ViewModel parent, wf.WFDefinition obj)
+            : base(appCtx, dataCtx, parent, obj)
+        {
+            WFDefinition = obj;
+        }
+
+        public wf.WFDefinition WFDefinition { get; private set; }
+
+        protected override List<PropertyGroupViewModel> CreatePropertyGroups()
+        {
+            var result = base.CreatePropertyGroups();
+
+            result.Add(ViewModelFactory.CreateViewModel<SinglePropertyGroupViewModel.Factory>().Invoke(DataContext, this, "Designer", new[] { 
+                ViewModelFactory.CreateViewModel<LabeledViewContainerViewModel.Factory>().Invoke(DataContext, this, "Workflow", "", 
+                    ViewModelFactory.CreateViewModel<WFDefinitionDesigner.Factory>().Invoke(DataContext, this, WFDefinition))
+            }));
+
+            return result;
+        }
+    }
+}
