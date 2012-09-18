c1e2a8e198aba6994addf67010d09e551e01c9fd
Arthur Zaczek <arthur@dasz.at>
2012-09-18 14:58:09 +0200

    Tuning GraphLayout

--- zetbox.Workflow.Client.WPF/View/Workflow/Designer/GraphLayout.cs
+++ zetbox.Workflow.Client.WPF/View/Workflow/Designer/GraphLayout.cs
@@ -11,10 +11,35 @@ namespace zetbox.Workflow.Client.WPF.View.Workflow.Designer
     [CLSCompliant(false)]
     public class WFDefinitionGraphLayout : GraphLayout<StateDefinitionGraphViewModel, IEdge<StateDefinitionGraphViewModel>, WFDefinitionGraph>
     {
+        public WFDefinitionGraphLayout()
+        {
+            this.LayoutAlgorithmType = "Tree";
+            var p = this.LayoutParameters as GraphSharp.Algorithms.Layout.Simple.Tree.SimpleTreeLayoutParameters;
+            if (p != null)
+            {
+                p.Direction = GraphSharp.Algorithms.Layout.LayoutDirection.LeftToRight;
+            }
+            this.OverlapRemovalAlgorithmType = "FSA";
+            ((GraphSharp.Algorithms.OverlapRemoval.OverlapRemovalParameters)this.OverlapRemovalParameters).HorizontalGap = 40f;
+            ((GraphSharp.Algorithms.OverlapRemoval.OverlapRemovalParameters)this.OverlapRemovalParameters).VerticalGap = 20f;
+        }
     }
 
     [CLSCompliant(false)]
     public class StateDefinitionGraphLayout : GraphLayout<object, IEdge<object>, StateDefinitionGraph>
     {
+        public StateDefinitionGraphLayout()
+        {
+            this.LayoutAlgorithmType = "Tree";
+            var p = this.LayoutParameters as GraphSharp.Algorithms.Layout.Simple.Tree.SimpleTreeLayoutParameters;
+            if (p != null)
+            {
+                p.Direction = GraphSharp.Algorithms.Layout.LayoutDirection.LeftToRight;
+            }
+
+            this.OverlapRemovalAlgorithmType = "FSA";
+            ((GraphSharp.Algorithms.OverlapRemoval.OverlapRemovalParameters)this.OverlapRemovalParameters).HorizontalGap = 40f;
+            ((GraphSharp.Algorithms.OverlapRemoval.OverlapRemovalParameters)this.OverlapRemovalParameters).VerticalGap = 20f;
+        }
     }
 }
