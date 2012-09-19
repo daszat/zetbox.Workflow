63ef48913d6e8c3ff0729576c1150170c2be6d8c
Arthur Zaczek <arthur@dasz.at>
2012-09-19 18:12:17 +0200

    first invocation

--- zetbox.Workflow.Client/Workflow/CommonInvocations/Action.cs
+++ zetbox.Workflow.Client/Workflow/CommonInvocations/Action.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace zetbox.Workflow.Client.Workflow.CommonInvocations
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using wf = Zetbox.Basic.Workflow;
+    using Zetbox.App.Base;
+    
     public static class Action
     {
+        public static bool Forward(wf.Action action, wf.State current, Identity identity)
+        {
+            var ctx = action.Context;
+            var logEntry = ctx.Create<wf.LogEntry>();
+            logEntry.Message = "Was forwared...";
+            current.Instance.LogEntries.Add(logEntry);
+
+            return true;
+        }
     }
 }
