84e6f9dc4a3a40c3188c23284dc5b6a9d5655489
Arthur Zaczek <arthur@dasz.at>
2012-09-19 17:41:33 +0200

    Setting up typerefs and classes for custom workflow action and state change logic

---
+++ zetbox.Workflow.Common/Workflow/CommonInvocations/Action.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zetbox.Workflow.Common.Workflow.CommonInvocations
+{
+    public static class Action
+    {
+    }
+}
