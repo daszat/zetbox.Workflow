fd92908c87398e0e83111af15d72e50f077f7aaf
Arthur Zaczek <arthur@dasz.at>
2012-09-21 11:29:17 +0200

    Implemented Forward Action.

--- zetbox.Workflow.Client/Workflow/CommonInvocations/Action.cs
+++ zetbox.Workflow.Client/Workflow/CommonInvocations/Action.cs
@@ -7,22 +7,46 @@ namespace zetbox.Workflow.Client.Workflow.CommonInvocations
     using System.Text;
     using wf = Zetbox.Basic.Workflow;
     using Zetbox.App.Base;
-using Zetbox.Client.Presentables;
-    
+    using Zetbox.Client.Presentables;
+    using Zetbox;
+    using Zetbox.API;
+
     public class Action
     {
         private readonly IViewModelFactory _vmf;
+        private readonly IFrozenContext _frozenCtx;
 
-        public Action(IViewModelFactory vmf)
+        public Action(IViewModelFactory vmf, IFrozenContext frozenCtx)
         {
             _vmf = vmf;
+            _frozenCtx = frozenCtx;
         }
 
         public bool Forward(wf.Action action, wf.State current, Identity identity)
         {
-            _vmf.ShowMessage("Hello from Action", "Hello");
-            current.Instance.AddLogEntry("Was forwared to ");
-            return true;
+            var ctx = current.Context;
+            var retVal = false;
+            var dlg = _vmf.CreateViewModel<DataObjectSelectionTaskViewModel.Factory>()
+                .Invoke(
+                    ctx,
+                    _vmf.GetWorkspace(ctx),
+                    (ObjectClass)NamedObjects.Base.Classes.Zetbox.App.Base.Identity.Find(_frozenCtx),
+                    null,
+                    (lst) =>
+                    {
+                        foreach (Identity id in lst.Select(i => i.Object))
+                        {
+                            current.Instance.AddLogEntry("Forwared to " + id.ToString());
+                            if (!current.Persons.Contains(id))
+                            {
+                                current.Persons.Add(id);
+                            }
+                        }
+                        retVal = true;
+                    },
+                    null);
+            _vmf.ShowDialog(dlg);
+            return retVal;
         }
     }
 }
