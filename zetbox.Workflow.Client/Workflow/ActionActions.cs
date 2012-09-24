bfcea8acb4e5e6937667b25b6ee1b09c748c4ee8
Arthur Zaczek <arthur@dasz.at>
2012-09-24 10:41:39 +0200

    Initialize Action with default param definition

---
+++ zetbox.Workflow.Client/Workflow/ActionActions.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Zetbox.Basic.Workflow
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Zetbox.API;
+    using System.Text.RegularExpressions;
+    using Zetbox.API.Common;
+    using Zetbox.App.Base;
+
+    [Implementor]
+    public static class ActionActions
+    {
+        [Invocation]
+        public static void NotifyCreated(Action obj)
+        {
+            obj.ParameterType = (ObjectClass)NamedObjects.Base.Classes.Zetbox.Basic.Workflow.ParameterizedActionDefinition.Find(obj.Context);
+        }
+    }
+}
