c375021c31f70262c878064fe84d845c3388c0c9
Arthur Zaczek <arthur@dasz.at>
2012-09-24 10:40:44 +0200

    Initialize SchedulerActions with the default implementor

--- zetbox.Workflow.Client/Workflow/SchedulerActionActions.cs
+++ zetbox.Workflow.Client/Workflow/SchedulerActionActions.cs
@@ -9,6 +9,7 @@ namespace Zetbox.Basic.Workflow
     using System.Text.RegularExpressions;
     using Zetbox.API.Common;
     using Zetbox.App.Base;
+    using Zetbox.App.Extensions;
 
     [Implementor]
     public static class SchedulerActionActions
@@ -17,6 +18,8 @@ namespace Zetbox.Basic.Workflow
         public static void NotifyCreated(SchedulerAction obj)
         {
             obj.ParameterType = (ObjectClass)NamedObjects.Base.Classes.Zetbox.Basic.Workflow.ScheduledActionDefinition.Find(obj.Context);
+            obj.Implementor = typeof(zetbox.Workflow.Common.Workflow.CommonInvocations.Action).ToRef(obj.Context);
+            obj.MemberName = "Schedule";
         }
     }
 }
