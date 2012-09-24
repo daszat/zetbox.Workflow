182677ea1c875d52945a2840a259d85e86ec6631
Arthur Zaczek <arthur@dasz.at>
2012-09-24 18:02:15 +0200

    WFInstance has its own view model now, new test data

---
+++ zetbox.Workflow.Client/ViewModel/Workflow/WFInstanceViewModel.cs
@@ -0,0 +1,29 @@
+namespace zetbox.Workflow.Client.ViewModel.Workflow
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Zetbox.Client.Presentables;
+    using Zetbox.API;
+    using wf = Zetbox.Basic.Workflow;
+
+    [ViewModelDescriptor]
+    public class WFInstanceViewModel : DataObjectViewModel
+    {
+        public new delegate WFInstanceViewModel Factory(IZetboxContext dataCtx, ViewModel parent, IDataObject obj);
+
+        public WFInstanceViewModel(IViewModelDependencies appCtx, IZetboxContext dataCtx, ViewModel parent, wf.WFInstance obj)
+            : base(appCtx, dataCtx, parent, obj)
+        {
+            Instance = obj;
+        }
+
+        public wf.WFInstance Instance { get; private set; }
+
+        public override string Name
+        {
+            get { return Instance.ToString(); }
+        }
+    }
+}
