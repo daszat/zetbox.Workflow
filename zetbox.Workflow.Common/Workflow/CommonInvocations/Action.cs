9d265df21b9d127a41148e2b63cc47307f86c03e
Arthur Zaczek <arthur@dasz.at>
2012-09-26 15:57:21 +0200

    Using recurrence CP-Obj now

--- zetbox.Workflow.Common/Workflow/CommonInvocations/Action.cs
+++ zetbox.Workflow.Common/Workflow/CommonInvocations/Action.cs
@@ -36,30 +36,8 @@ Best regards, your Workflow System");
             var sAction = (wf.SchedulerAction)action;
             var sParameter = (wf.ScheduledActionDefinition)parameter;
 
-            DateTime dt = DateTime.Now;
-
-            if (sParameter.EveryYear.HasValue)
-            {
-                dt = dt.FirstYearDay().AddYears(1);
-            }
-            else if (sParameter.EveryQuater.HasValue)
-            {
-                dt = dt.FirstQuaterDay().AddMonths(3);
-            }
-            else if (sParameter.EveryMonth.HasValue)
-            {
-                dt = dt.FirstMonthDay().AddMonths(1);
-            }
-            else if (sParameter.EveryDay.HasValue)
-            {
-                dt = dt.Date;
-            }
-
-            dt = dt
-                .AddMonths(sParameter.Months ?? sAction.Months ?? 0)
-                .AddDays(sParameter.Days ?? sAction.Days ?? 0)
-                .AddHours(sParameter.Hours ?? sAction.Hours ?? 0)
-                .AddMinutes(sParameter.Minutes ?? sAction.Minutes ?? 0);
+            DateTime dt = sAction.Schedule.GetNext();
+            dt = sParameter.Schedule.GetNext(dt);
 
             current.ScheduleAction(dt, sParameter.InvokeAction);
 
