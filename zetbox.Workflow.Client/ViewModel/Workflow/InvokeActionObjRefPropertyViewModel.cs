4911c6fdbef3126367662c59dd447ee00b2200d9
Arthur Zaczek <arthur@dasz.at>
2012-11-14 16:50:39 +0100

    upgrade to latest zetbox version

--- zetbox.Workflow.Client/ViewModel/Workflow/InvokeActionObjRefPropertyViewModel.cs
+++ zetbox.Workflow.Client/ViewModel/Workflow/InvokeActionObjRefPropertyViewModel.cs
@@ -8,6 +8,7 @@ namespace zetbox.Workflow.Client.ViewModel.Workflow
     using Zetbox.API;
     using Zetbox.Client.Presentables.ValueViewModels;
     using Zetbox.Client.Models;
+    using Zetbox.API.Async;
 
     [ViewModelDescriptor]
     public class InvokeActionObjRefPropertyViewModel : ObjectReferenceViewModel
@@ -19,9 +20,8 @@ namespace zetbox.Workflow.Client.ViewModel.Workflow
         {
         }
 
-        protected override List<ViewModel> GetPossibleValues(out bool needMoreButton)
+        protected override ZbTask<Tuple<List<ViewModel>, bool>> GetPossibleValuesAsync()
         {
-            needMoreButton = false;
             var result = new List<ViewModel>();
             var propMdl = base.ObjectReferenceModel as ObjectReferencePropertyValueModel;
             if (propMdl != null)
@@ -29,7 +29,7 @@ namespace zetbox.Workflow.Client.ViewModel.Workflow
                 var obj = (Zetbox.Basic.Workflow.ScheduledActionDefinition)propMdl.Object;
                 result.AddRange(obj.StateDefinition.Actions.Select(i => DataObjectViewModel.Fetch(ViewModelFactory, DataContext, ViewModelFactory.GetWorkspace(DataContext), i)).Cast<ViewModel>());
             }
-            return result;
+            return new ZbTask<Tuple<List<ViewModel>, bool>>(new Tuple<List<ViewModel>, bool>(result, false));
         }
     }
 }
